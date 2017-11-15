956abe82a983779daa86a24a36feee18fb374a07
denimkqqa <denimkqqa@gmail.com>
2017-11-15 22:13:01 +0200

    Renamed example project

--- AppExample/src/main/java/space/atata/compassexample/MainFragment.kt
+++ AppExample/src/main/java/space/atata/compassexample/MainFragment.kt
@@ -10,7 +10,6 @@ import space.atata.compassexample.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         binding = FragmentMainBinding.inflate(inflater, container, false)
